df5412a560e117fe01648932d2ac294dbc60bb7c
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-04 08:55:46 +0900

    Update plus.c

--- day1/plus.c
+++ day1/plus.c
@@ -6,17 +6,17 @@ void main()
 {
   int num = 0;
   scanf("%d", &num);
-  if(num < 100){
-      printf("100 미만으로 입력하세요\n");
+  if(num <= 999){
+    int a = num / 100;
+    int b = (num - (a * 100)) / 10;
+    int c = (num) % 10;
+  
+    int result = a + b + c;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", result);
+  } else {
+    printf("백의 자리수를 넘겼습니다.\n");
   }
-  
-  int a = num / 100;
-  int b = (num - (a * 100)) / 10;
-  int c = (num) % 10;
-  
-  int result = a + b + c;
-  printf("%d\n", a);
-  printf("%d\n", b);
-  printf("%d\n", c);
-  printf("%d\n", result);
 }
