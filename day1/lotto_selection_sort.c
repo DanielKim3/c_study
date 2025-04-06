6204ed16b0c4780514dc42df13d1478680c53102
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-06 22:19:11 +0900

    Update lotto_selection_sort.c

--- day1/lotto_selection_sort.c
+++ day1/lotto_selection_sort.c
@@ -6,33 +6,42 @@
 
 int main()
 {
-  int container[6];
-  int container3[6];
-  int min = 46;
-  
- for(int i = 0; i < 6; i++){
+    int container[6];
+    int min, temp;
     srand(time(NULL));
-    int num = (int)(rand() % 45) + 1;
-    sleep(1); // 1초
-    container[i] = num;
-  }
-  
-  for(int i = 0; i < 6; i++){
-    for(int j = 0; j < 6; j++){
-        int bools = 0;
-        for(int k = 0; k < i; k++) {
-            if(container[j] == container3[k]){
-                bools = 1;
-                break;
+
+    for (int i = 0; i < 6; i++) {
+        int num = rand() % 45 + 1;
+        int copy = 1;
+
+        while (copy) {
+            copy = 0;
+            num = rand() % 45 + 1;
+            
+            for (int j = 0; j < i; j++) {
+                if (container[j] == num) {
+                    copy = 1;
+                    break;
+                }
             }
         }
-        if(container[j] < min && bools == 0){
-            min = container[j];
-        }
+        container[i] = num;
     }
-    container3[i] = min;
-    min = 46;
-    printf("for문 %d\n", container3[i]);
-  }
-  return 0;
+    
+    for (int i = 0; i < 6; i++) {
+        min = i;
+        for (int j = i + 1; j < 6; j++) {
+            if (container[min] > container[j]) {
+                min = j;
+            }
+        }
+        temp = container[i];
+        container[i] = container[min];
+        container[min] = temp;
+
+        printf("for문 %d\n", container[i]);
+    }
+
+    return 0;
 }
+
