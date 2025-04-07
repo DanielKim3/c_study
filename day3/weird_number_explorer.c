3264f8771387246eb7dcb6a640accfc143bedd79
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-07 17:57:59 +0900

    Update weird_number_explorer.c

--- day3/weird_number_explorer.c
+++ day3/weird_number_explorer.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+
+int contains(int num) {
+    char str[20];
+    sprintf(str, "%d", num); // 숫자를 문자열로 변환
+    return strchr(str, '7') != NULL;
+}
 
 int main()
 {
-    int m, n = 0;
-    printf("정수 입력 \n");
+    int m, n;
+    printf("정수 입력 (두 개):\n");
     scanf("%d %d", &m, &n);
     printf("===============\n");
-    if (m > 0 & n > 0 ) {
-        for (int i = m; m <= n; m++) {
-            if (m % 7 == 0 | m == 7) {
+
+    if (m > 0 && n > 0) {
+        for (int i = m; i <= n; i++) {
+            if (i % 7 == 0 || contains(i)) {
                 continue;
-            } else if (m % 5 == 0) {
-                printf("%d은 5의 배수 입니다.", m);
+            } else if (i % 5 == 0) {
+                printf("%d은 5의 배수 입니다.\n", i);
                 break;
             } else {
-                printf("%d\n", m);
+                printf("%d\n", i);
             }
         }
     } else {
-        printf("1이상 작성 하시오.");
+        printf("1 이상 작성하시오.\n");
     }
     printf("\n");
+
+    return 0;
 }
