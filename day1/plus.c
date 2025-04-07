b38c333ae82d843413102c424b9e19169a1eb722
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-07 10:58:24 +0900

    Update plus.c

--- day1/plus.c
+++ day1/plus.c
@@ -9,9 +9,9 @@ void main()
   if(num <= 999){
     int a = num / 100;
     int b = (num - (a * 100)) / 10;
-    int c = (num) % 10;
-  
+    int c = (num) % 10;  
     int result = a + b + c;
+    
     printf("%d\n", a);
     printf("%d\n", b);
     printf("%d\n", c);
