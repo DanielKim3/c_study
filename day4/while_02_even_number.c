e467be66a6ad678913c428307bd2aa78fa10a4ff
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-08 10:31:21 +0900

    Create while_02_even_number.c

---
+++ day4/while_02_even_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 0;
+    printf("정수를 입력하세요");
+    scanf("%d", &num);
+    if(num != 0 || num > 0) { 
+        int i = 0;
+        while (i < num) {
+            i++;
+            if(i % 2 == 1) {
+                continue;
+            }
+            printf("%d ", i);
+        }
+    } else {
+        printf("1 이상 숫자를 정수를 입력하세요");
+    }
+}
