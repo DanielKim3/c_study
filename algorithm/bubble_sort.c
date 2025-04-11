65d75a4380cc08c86b96404752653db96ba20059
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-11 23:55:41 +0900

    Create bubble_sort.c

---
+++ algorithm/bubble_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int container[6];
+    int min, temp;
+    srand(time(NULL));
+
+    for (int i = 0; i < 6; i++) {
+        int num;
+        int copy = 1;
+
+        while (copy) {
+            copy = 0;
+            num = rand() % 45 + 1;
+            
+            for (int j = 0; j < i; j++) {
+                if (container[j] == num) {
+                    copy = 1;
+                    break;
+                }
+            }
+        }
+        container[i] = num;
+        printf("%d ", container[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 5 - i; j++) {
+            if (container[j] > container[j + 1]) {
+                temp = container[j];
+                container[j] = container[j + 1];
+                container[j + 1] = temp;
+            }
+        }
+    }
+    
+    for (int i = 0; i < 6; i++) {
+        printf("for문 %d\n", container[i]);
+    }
+}
