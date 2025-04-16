6e027395860f9211cf78ad1bec379eecbd5eda43
DanielKim3 <85281109+DanielKim3@users.noreply.github.com>
2025-04-17 08:57:11 +0900

    Update union.c

--- day6/union.c
+++ day6/union.c
@@ -15,11 +15,8 @@ void main(void)
     printf("int 값: %d, 주소: %p\n", u.intVal, (void *)&u.intVal);
     printf("실수를 입력하세요: ");
     scanf("%f", &u.floatVal);
-    printf("int 값: %d, 주소: %p\n", u.intVal, (void *)&u.intVal);
     printf("float 값: %f, 주소: %p\n", u.floatVal, (void *)&u.floatVal);
     printf("문자열을 입력하세요: ");
     scanf("%s", u.charVal);
-    printf("int 값: %d, 주소: %p\n", u.intVal, (void *)&u.intVal);
-    printf("float 값: %f, 주소: %p\n", u.floatVal, (void *)&u.floatVal);
     printf("char 값: %s, 주소: %p\n", u.charVal, (void *)u.charVal);
 }
